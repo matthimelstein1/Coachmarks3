11c7915aead1530aff9ab732c75a60e0261e8463
Swoopt <matt.himelstein@gmail.com>
2015-04-03 16:07:06 -0700

    Initial Commit

---
+++ Coachmarks3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coachmarks3
+//
+//  Created by Swoopt on 4/3/15.
+//  Copyright (c) 2015 Heavenstone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
